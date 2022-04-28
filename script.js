2a7c3f926a3d8a027463dd0322997148e35ca0ce
swarajkaran <swarajkaran.raj@gmail.com>
2022-04-29 01:45:08 +0530

    Building Promise

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
-*/
+
 
 // Event Loop
 console.log('Test start');
@@ -224,3 +224,31 @@ Promise.resolve('Resolve promise 2').then(res => {
   console.log(res);
 });
 console.log('Test end');
+*/
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN!!');
+    } else {
+      reject(new Error('You lost'));
+    }
+  }, 2000);
+});
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// Promisifying setTimeout
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 secinds'));
