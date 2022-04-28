22a7309d19d913b890bdd265ca06dc22317235e7
swarajkaran <swarajkaran.raj@gmail.com>
2022-04-29 01:01:38 +0530

    event loop in practice

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ TEST COORDINATES 2: 19.037, 72.873
 TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
-*/
+
 
 const whereAmI = function (lat, lng) {
   const url = `https://geocode.xyz/${lat},${lng}?json=1`;
@@ -212,3 +212,10 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+// Event Loop
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Result promise 1').then(res => console.log(res));
+console.log('Test end');
