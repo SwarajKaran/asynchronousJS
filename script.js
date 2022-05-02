549e3bac5572293fc868d0b20b2a97294cf87caf
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-02 23:28:50 +0530

    load challenge 3

--- script.js
+++ script.js
@@ -482,7 +482,7 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('usa', 'australia', 'tanzania');
-*/
+
 // 3 other promise combinators
 
 // 1. Promise.race
@@ -531,3 +531,23 @@ Promise.any([
   Promise.reject('Error'),
   Promise.resolve('Success'),
 ]).then(res => console.log(res));
+*/
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
