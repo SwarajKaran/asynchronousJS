f1b779cc374b5bf9204618d587e5a29b01a14887
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-05 23:24:02 +0530

    challenge-part2

--- script.js
+++ script.js
@@ -567,7 +567,7 @@ const createImage = function (imgPath) {
 
 let currImage;
 
-const loadAll = async function () {
+const loadNPause = async function () {
   try {
     let img = await createImage('./img/img-1.jpg');
     console.log('Image 1 loaded');
@@ -583,7 +583,7 @@ const loadAll = async function () {
   }
 };
 
-loadAll();
+// loadNPause();
 
 // createImage('./img/img-1.jpg')
 //   .then(img => {
@@ -601,3 +601,16 @@ loadAll();
 //     return wait(2);
 //   })
 //   .then(() => (currImage.style.display = 'none'));
+//PART 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.log(err);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
